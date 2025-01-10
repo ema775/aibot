72e298f50c451551676443b195de42a66322dce1
ema775 <ema0775.main@gmail.com>
2025-01-11 03:34:48 +0900

    v0.1.0

---
+++ src/aibot/_version.py
@@ -0,0 +1,11 @@
+__title__ = "aibot"
+
+# Use Semantic Versioning format: MAJOR.MINOR.PATCH
+# Semantic Versioning (セマンティックバージョニング) の形式を使用: MAJOR.MINOR.PATCH
+# MAJOR - Backward-incompatible changes
+# MAJOR - 後方互換性のない変更
+# MINOR - Backward-compatible feature additions
+# MINOR - 後方互換性を保ちながら機能追加
+# PATCH - Bug fixes or minor improvements
+# PATCH - バグ修正や小さな改良
+__version__ = "0.1.0"
