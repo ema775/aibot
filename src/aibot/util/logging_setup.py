f98533bdf8f1d85db67cbeb7560b1b636f3a0ee7
ema775 <ema0775.main@gmail.com>
2025-01-11 12:32:43 +0900

    feat(logging): Add logging setup function for configurable logging

--- src/aibot/util/logging_setup.py
+++ src/aibot/util/logging_setup.py
@@ -1,13 +1,27 @@
+import inspect
 import logging
-
-LOG_FILE_NAME = "gptbot.log"
-LOG_FORMAT = "%(asctime)s | [%(levelname)s] | [%(filename)s:ln%(lineno)d] %(message)s"
-DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
-
-logging.basicConfig(
-    filename=LOG_FILE_NAME,
-    format=LOG_FORMAT,
-    level=logging.INFO,
-)
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
+
+
+def setup_logging(log_level: int) -> None:
+    """Set up the logging configuration.
+
+    Parameters
+    ----------
+    log_level : int, optional
+        The logging level (e.g., logging.DEBUG, logging.INFO).
+
+    """
+    caller_frame = inspect.stack()[1]
+    caller_file = Path(caller_frame.filename).stem + ".log"
+
+    logging.basicConfig(
+        level=log_level,  # ログレベルを引数で設定
+        format="%(asctime)s | [%(levelname)s] | [%(filename)s:ln%(lineno)d] %(message)s",
+        handlers=[
+            logging.FileHandler(caller_file),  # ファイルにログを出力
+            logging.StreamHandler(),  # コンソールにログを出力
+        ],
+    )
